34abe25c8b76b4df8413147bd1c759ec56d9ee53
Anshul <anshul.tripathi@travclan.com>
2022-08-25 14:33:23 +0530

    timer done

--- app/src/main/java/com/example/fragmentcommunication/BottomFragment.java
+++ app/src/main/java/com/example/fragmentcommunication/BottomFragment.java
@@ -6,16 +6,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class BottomFragment extends Fragment {
     private static final String TAG = "BottomFragment";
     public static final String TIMER_DURATION_ARG = "timer_duration_arg";
-    private TextView mTimerTV;
+    private TextView timerTV;
+    private Button startTimerButton;
 
     public BottomFragment() {
         // Required empty public constructor
@@ -37,7 +40,23 @@ public class BottomFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        mTimerTV = (TextView) getView().findViewById(R.id.time_tv);
+        timerTV = (TextView) view.findViewById(R.id.time_tv);
+        startTimerButton = (Button) view.findViewById(R.id.start_timer_button);
+        startTimerButton.setOnClickListener(button -> {
+            long totalTime = Long.parseLong(timerTV.getText().toString()) * 1000 + 1000;
+            new CountDownTimer(totalTime, 1000) {
+                @Override
+                public void onFinish() {
+                    timerTV.setText("0");
+                }
+
+                @Override
+                public void onTick(long l) {
+                    int timeInSeconds = (int) l / 1000;
+                    timerTV.setText(String.valueOf(timeInSeconds));
+                }
+            }.start();
+        });
     }
 
     @Override
@@ -48,7 +67,7 @@ public class BottomFragment extends Fragment {
         if (args != null) {
             String timerDuration = args.getString(TIMER_DURATION_ARG);
             Log.d(TAG, "onStart: " + timerDuration);
-            mTimerTV.setText(timerDuration);
+            timerTV.setText(timerDuration);
         }
     }
 }
