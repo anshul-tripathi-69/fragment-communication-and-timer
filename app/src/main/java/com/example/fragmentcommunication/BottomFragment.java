d1103251e4bfc90e52091e840858395b9b91d2cb
Anshul <anshul.tripathi@travclan.com>
2022-08-26 13:52:21 +0530

    done

--- app/src/main/java/com/example/fragmentcommunication/BottomFragment.java
+++ app/src/main/java/com/example/fragmentcommunication/BottomFragment.java
@@ -2,13 +2,20 @@ package com.example.fragmentcommunication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class BottomFragment extends Fragment {
+    TextView mTimerTv;
 
     public BottomFragment() {
         // Required empty public constructor
@@ -17,6 +24,15 @@ public class BottomFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getParentFragmentManager().setFragmentResultListener(
+            "timer_duration_request_key",
+            this,
+            (requestKey, result) -> {
+                String timerDuration = result.getString("timer_duration");
+                mTimerTv.setText(timerDuration);
+            }
+        );
     }
 
     @Override
@@ -24,5 +40,12 @@ public class BottomFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bottom, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mTimerTv = view.findViewById(R.id.time_tv);
     }
 }
--- app/src/main/java/com/example/fragmentcommunication/TopFragment.java
+++ app/src/main/java/com/example/fragmentcommunication/TopFragment.java
@@ -2,13 +2,19 @@ package com.example.fragmentcommunication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TopFragment extends Fragment {
+    Button mNextButton;
+    EditText mEditText;
 
     public TopFragment() {
         // Required empty public constructor
@@ -24,5 +30,18 @@ public class TopFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_top, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mEditText = view.findViewById(R.id.editTextNumber);
+        mNextButton = view.findViewById(R.id.next_button);
+        mNextButton.setOnClickListener((button) -> {
+            Bundle result = new Bundle();
+            result.putString("timer_duration", mEditText.getText().toString());
+            getParentFragmentManager().setFragmentResult("timer_duration_request_key", result);
+        });
     }
 }
